9a37450a6eeecce1993d7b230a72bbaed5c5e36f
Adam <adambacha95@gmail.com>
2025-02-28 16:55:47 +0100

    test au cas ou

---
+++ zfichertest/testdeProjet/test2.js
@@ -0,0 +1,59 @@
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// menu burger
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+
+
+function Burger(){
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    overlay.classList.add('fade-in-element');
+    console.log('click sur le bouton burger')
+});
+
+overlay.addEventListener('click', () => {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    overlay.classList.add('fade-out-element');
+    console.log("click sur l'overlay")
+});
+
+console.log('on est a la fin')
+
+}
+
+Burger();
+
+// function transitionOverlay(){
+    
+// }
+
+// transitionOverlay();
+
+
+// fin menu Burger
+
+// flecheRetour
+
+const flecheRetour = document.getElementById('flecheRetour');
+flecheRetour.addEventListener('click', () => {
+    window.history.back();
+    console.log('fleche retour a fonctionné')
+});
+
+
+// fin flecheRetour
